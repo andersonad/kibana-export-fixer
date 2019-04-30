89f3b4ed637737caed2997ad9b98463abe15921f
Austin Anderson <adanderson@prominentedge.com>
2019-04-30 13:03:18 -0500

    initial commit

---
+++ kibana-export-fixer.js
@@ -0,0 +1,23 @@
+//takes 2 arguments: first is output file directory, second is input file
+const fs = require("fs");
+// process.argv.forEach(function (val, index, array) {
+//   console.log(index + ': ' + val);
+// });
+
+const outputDirectoryPath = process.argv[2];
+const jsonFilename = process.argv[3];
+const contents = fs.readFileSync(jsonFilename);
+const jsonContent = JSON.parse(contents);
+const newContent = jsonContent[0];
+
+newContent._index = ".kibana_{{ FireDepartment.firecares_id }}";
+newContent._type = "doc";
+newContent._id = ["visualization:",jsonFilename.split('.json')[0]].join('');
+const oldSource = newContent._source;
+newContent._source = {};
+newContent._source.type = "visualization";
+newContent._source.visualization = oldSource;
+fs.writeFile(["./",jsonFilename.split('.json')[0],"_fixed.json"].join(''), JSON.stringify(newContent,null,2), (err) => {
+  if (err) throw err;
+  console.log(['Fixed file output to ', outputDirectoryPath,jsonFilename].join(''));
+});
