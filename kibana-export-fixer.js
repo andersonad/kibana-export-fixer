5024645ef0b7dc2075384c61a0ca6b7ba2eaca8f
Austin Anderson <adanderson@prominentedge.com>
2019-04-30 13:30:56 -0500

    add support for selecting output directory to support xargs calls

--- kibana-export-fixer.js
+++ kibana-export-fixer.js
@@ -1,12 +1,13 @@
 //takes 2 arguments: first is output file directory, second is input file
 const fs = require("fs");
+const path = require("path");
 // process.argv.forEach(function (val, index, array) {
 //   console.log(index + ': ' + val);
 // });
 
 const outputDirectoryPath = process.argv[2];
-const jsonFilename = process.argv[3];
-const contents = fs.readFileSync(jsonFilename);
+const jsonFilename = path.basename(process.argv[3]);
+const contents = fs.readFileSync(process.argv[3]);
 const jsonContent = JSON.parse(contents);
 const newContent = jsonContent[0];
 
@@ -17,7 +18,7 @@ const oldSource = newContent._source;
 newContent._source = {};
 newContent._source.type = "visualization";
 newContent._source.visualization = oldSource;
-fs.writeFile(["./",jsonFilename.split('.json')[0],"_fixed.json"].join(''), JSON.stringify(newContent,null,2), (err) => {
+fs.writeFile([outputDirectoryPath,jsonFilename.split('.json')[0],"_fixed.json"].join(''), JSON.stringify(newContent,null,2), (err) => {
   if (err) throw err;
   console.log(['Fixed file output to ', outputDirectoryPath,jsonFilename].join(''));
 });
